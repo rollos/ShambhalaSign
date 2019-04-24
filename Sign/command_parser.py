febfb1903fd09082cf8f429d44721f4d8efa5fd2
Woodrow Melling <woodrow@schedulicity.com>
2019-04-24 08:29:45 -0600

    Add command parser for phrase vs artist

--- Sign/command_parser.py
+++ Sign/command_parser.py
@@ -4,4 +4,19 @@ class CommandParser:
     def parse_data(data):
         decoded = "".join(map(chr, data))
         print("CommandParser: {}".format(decoded))
+
+        data = decoded.split(":")
+
+        command = data[0]
+        content = data[1]
+
+        if command == "phrase":
+            return content
+        elif command == "artist":
+            return "Artist: " + content
+        else:
+            print("unrecognized command")
+
+
+
         return decoded
